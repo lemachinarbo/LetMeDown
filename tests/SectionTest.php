df12b209d129bc5781dbb6a37646d7da9ddc1598
Lemachi Barno <michael@diptongonante.com>
2026-05-09 23:17:16 -0500

    chore: finalize conductor skill and add section key tests

--- tests/SectionTest.php
+++ tests/SectionTest.php
@@ -32,4 +32,27 @@ MD;
 
         unlink($tmp);
     }
+
+    public function test_named_section_key_is_correct()
+    {
+        $markdown = "<!-- section:my_section -->\n# Heading";
+        $content = $this->getParser()->loadFromString($markdown);
+        $this->assertEquals('my_section', $content->sections[0]->key);
+    }
+
+    public function test_section_key_is_stable_after_data_projection()
+    {
+        $markdown = "<!-- section:my_section -->\n# Heading";
+        $content = $this->getParser()->loadFromString($markdown);
+        $section = $content->sections[0];
+        
+        $data = $content->data();
+        $this->assertEquals('my_section', $data['my_section']['key']);
+        $this->assertEquals('my_section', $section->key);
+    }
+
+    private function getParser(): LetMeDown
+    {
+        return new LetMeDown(sys_get_temp_dir());
+    }
 }
